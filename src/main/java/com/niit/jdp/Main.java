bc45525e288c79e1e4bdcfbde5bff8ba59f2312b
Pinky Sodhi <pinky.sodhi@niit.com>
2022-11-01 17:41:11 +0530

    Coded the logic to find out a lucky winner inside main() method

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -1,7 +1,22 @@
 package com.niit.jdp;
 
+import javax.swing.*;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] names = {"Ajay Sharma", "Anisha Palei", "Ankit Kishore", "Ankit Ambekar",
+                "Ankush Singh", "Anuj Chaudhary", "Anusha S", "Ashutosh Das", "Bhagi Baghel",
+                "Bhosale Pratik Pratap", "Dinesh Kumar", "Gai Srikanth", "Hanamagouda Goudar",
+                "Harshal Urade", "Hemangi Thombre", "Hemosh Indorkar", "Ishu Bhatia",
+                "Joycee Alfred Nirmal", "Kaif", "Ketki Keni", "Masoom Vali",
+                "Mohd Adnan Parvez Shaikh Mukhtar", "Monica Ragunathan",
+                "Mushib Khan", "Naveen Kumar", "Sanket Gongale"};
+        System.out.println("There are " + names.length + " names in the list.");
+        Random random = new Random();
+        int randomIndex = random.nextInt(names.length);
+        System.out.println("Lucky Winner for the Question = " + names[randomIndex]);
+        JOptionPane.showConfirmDialog(null, "Lucky Winner is: " + names[randomIndex], "Winner", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, null);
+        JOptionPane.showInputDialog(null, "Enter your name");
     }
 }
